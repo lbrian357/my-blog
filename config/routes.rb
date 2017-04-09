86e5496ebc00a21086bf102b87a0e264823f6c49
Brian <lbrian357@gmail.com>
2017-04-09 17:34:17 -0400

    fixed links and add post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/update'
+
+  get 'posts/edit'
+
+  get 'posts/destroy'
+
+  get 'posts/index'
+
+  get 'posts/show'
+
   root 'clean_blogs#index'
   get 'about', to: 'clean_blogs#about'
   get 'post', to: 'clean_blogs#post'
